6ebb52a3202412ee13904f1cce10c8a712aebae8
Artem Davidovich <artem.davidovich@exactprosystems.com>
2020-10-01 17:05:18 +0400

    [TH2-269] Correct setup.py.

--- setup.py
+++ setup.py
@@ -75,10 +75,10 @@ class CustomDist(sdist):
         shutil.rmtree(package_name, ignore_errors=True)
 
 
-package_name = 'grpc-generator-template'
+package_name = environ['APP_NAME'] if 'APP_NAME' in environ else 'grpc-generator-template'
 
 setup(
-    name=environ['APP_NAME'] if 'APP_NAME' in environ else package_name,
+    name=package_name,
     version=environ['APP_VERSION'] if 'APP_VERSION' in environ else "1.0",
     install_requires=[
         'grpcio-tools',
