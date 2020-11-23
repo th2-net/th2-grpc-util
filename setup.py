8042f3b8453f439dcc7e797bb69f18bfef838caf
Sergey Raevsky <sergey.raevsky@exactprosystems.com>
2020-11-23 16:50:23 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common==2.3.0'
+        'th2-grpc-common==2.3.1'
     ],
     packages=['', package_name],
     package_data={'': ['package_info.json'], package_name: ['*.proto']},
