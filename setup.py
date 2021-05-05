8533e668cc651013b416a5be377e8560eee6be1e
Dmitry Karasev <dmitry.karasev@exactprosystems.com>
2021-05-05 12:32:04 +0400

    [th2-1515] removed dependency on Bintray (#9)

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common~=2.5.0'
+        'th2-grpc-common~=2.5.2'
     ],
     packages=packages,
     package_data=package_data,
