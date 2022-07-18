67987291f8cc9a2ac7d38e274feb894f7a88e785
Sofia Lobenko <sofia.lobenko@exactprosystems.com>
2022-07-18 09:55:55 +0000

    Update grpc-common version

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common>=3,<4',
+        'th2-grpc-common==4.0.0.dev2635598706',
         'mypy-protobuf==3.2'
     ],
     packages=packages,
